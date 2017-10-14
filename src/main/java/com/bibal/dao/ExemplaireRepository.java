5f40ca282e91f8b4af768f4ce6b356e659b26dba
BERTRAND Guillaume <guillaume5524@hotmail.fr>
2017-10-14 12:34:07 +0200

    fix:exemplaires:don't allow to loan Perime exemplaires

--- src/main/java/com/bibal/dao/ExemplaireRepository.java
+++ src/main/java/com/bibal/dao/ExemplaireRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public interface ExemplaireRepository extends JpaRepository<Exemplaire, Long> {
 
-    @Query(value = "SELECT * FROM exemplaire e WHERE e.id_exemplaire NOT IN (SELECT emp.id_exemplaire FROM emprunt emp)", nativeQuery = true)
+    @Query(value = "SELECT * FROM exemplaire e WHERE e.id_exemplaire NOT IN (SELECT emp.id_exemplaire FROM emprunt emp) AND etat<>'Périmé'", nativeQuery = true)
     List<Exemplaire> recupererTousLesExemplairesNonEmpruntes();
 }
